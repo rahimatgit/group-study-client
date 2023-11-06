d3e176e0f0e08358b8d539a938edca5e64116221
rahimatgit <rahimmonjur9@gmail.com>
2023-11-06 12:42:29 -0800

    navbar completed

--- src/Routes/MainLayout.jsx
+++ src/Routes/MainLayout.jsx
@@ -1,10 +1,60 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet} from "react-router-dom";
 
 
 const MainLayout = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <div className="drawer">
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <div className="w-full navbar bg-base-300">
+                        <div className="flex-none lg:hidden">
+                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+                        <div className="flex-1 px-2 mx-2">Navbar Title</div>
+                        <div className="flex-none hidden lg:block">
+                           
+                                {/* Navbar menu content here */}
+                                
+                                <div className="flex gap-2">
+                                <NavLink to="/" className="btn btn-primary btn-sm">
+                                    Home
+                                </NavLink>
+                                <NavLink to="/login" className="btn btn-primary btn-sm">
+                                    Login
+                                </NavLink>
+                                <NavLink to="/register" className="btn btn-primary btn-sm">
+                                    Register
+                                </NavLink>
+                                <NavLink to="/assignments" className="btn btn-primary btn-sm">
+                                    Assignments
+                                </NavLink>
+                                {/* <NavLink to="/assignments" className={({isActive}) =>}>
+                                    Assignments
+                                </NavLink>
+                                <NavLink className="btn btn-primary btn-sm">
+                                    logout
+                                </NavLink> */}
+                                </div>
+                            
+                        </div>
+                    </div>
+                    {/* Page content here */}
+                    <Outlet></Outlet>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
+            
         </div>
     );
 };
