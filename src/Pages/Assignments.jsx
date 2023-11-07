8401a295da5e5838410a65e413607c0a9b9b597c
rahimatgit <rahimmonjur9@gmail.com>
2023-11-07 12:19:48 -0800

    assignments data loading with axios is completed

--- src/Pages/Assignments.jsx
+++ src/Pages/Assignments.jsx
@@ -1,9 +1,19 @@
+import axios from "axios";
+import { useState } from "react";
 
 
 const Assignments = () => {
+
+    const [assignments, setAssignments] = useState([]);
+
+    axios.get('http://localhost:5000/assignments')
+    .then(res => {
+        setAssignments(res.data)
+    })
+
     return (
         <div>
-            this is assignments
+            <h2>Number of assignments: {assignments.length}</h2>
         </div>
     );
 };
