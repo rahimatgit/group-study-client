f887b0cdd4902105bf60cd62d107f3a09e2501fb
rahimatgit <rahimmonjur9@gmail.com>
2023-11-08 17:15:12 -0800

    FAQ section updated

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -12,28 +12,46 @@ const Faq = () => {
                 <div className="collapse collapse-plus bg-base-200">
                     <input type="radio" name="my-accordion-3" checked="checked" />
                     <div className="collapse-title text-xl font-medium">
-                        Click to open this one and close others
+                    What is AssignMate?
                     </div>
                     <div className="collapse-content">
-                        <p>hello</p>
+                        <p>AssignMate is an online platform designed to facilitate collaborative group study. It allows users from diverse countries to create and join assignments on a wide range of subjects and topics. AssignMate aims to provide a virtual environment where students and learners can collaborate, share knowledge, and enhance their understanding through group study sessions.</p>
                     </div>
                 </div>
                 <div className="collapse collapse-plus bg-base-200">
                     <input type="radio" name="my-accordion-3" />
                     <div className="collapse-title text-xl font-medium">
-                        Click to open this one and close others
+                    How can I create an assignment on AssignMate?
                     </div>
                     <div className="collapse-content">
-                        <p>hello</p>
+                        <p>Creating an assignment on AssignMate is simple. After signing up or logging in to your account, click on the "Create Assignment" button. You'll be prompted to fill in assignment details, such as the topic, description, and preferred study schedule. You can then invite other users to join your assignment, making it easy to coordinate study sessions with peers.</p>
                     </div>
                 </div>
                 <div className="collapse collapse-plus bg-base-200">
                     <input type="radio" name="my-accordion-3" />
                     <div className="collapse-title text-xl font-medium">
-                        Click to open this one and close others
+                    Is AssignMate free to use?
                     </div>
                     <div className="collapse-content">
-                        <p>hello</p>
+                        <p>Yes, AssignMate is free to use! We believe in providing accessible and inclusive study opportunities for everyone. There are no hidden costs or subscription fees. Simply sign up and start collaborating with fellow learners on our platform without any financial commitment.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-base-200">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-xl font-medium">
+                    Can I use AssignMate on my mobile device?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Absolutely! AssignMate is designed to be mobile-responsive. You can access our platform on your smartphone or tablet by visiting our website in your mobile browser. This allows you to participate in assignments, schedule study sessions, and communicate with your study group on the go.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-base-200">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-xl font-medium">
+                    How can I find assignments related to a specific topic or subject?
+                    </div>
+                    <div className="collapse-content">
+                        <p>To find assignments on a specific topic or subject, you can use our search feature. Simply enter keywords related to the topic you're interested in, and AssignMate will display a list of relevant assignments. You can also explore our "Browse Assignments" section to discover a wide variety of assignments across different subjects.</p>
                     </div>
                 </div>
             </div>
